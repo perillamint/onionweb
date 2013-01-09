5626e29ae93da0cf7c339ac470dac553d5198cd7
KBuild <qwer7995@gmail.com>
2013-01-10 00:36:16 +0900

    Add server start and stop

--- server.py
+++ server.py
@@ -1,9 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import string,cgi,time, os
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 class Server(BaseHTTPRequestHandler):
 	def do_GET(self):
 		try:
+			if self.path.endswith("/"):
+				print "read template"
+				f = open('template', 'r')
+				self.send_response(200)
+				readed = f.read()
+				self.wfile.write(readed)
+				f.close()
+				return
+
 			patharray = self.path.split('/')
 
 			sc = ServerConfig()
@@ -28,13 +40,25 @@ class Server(BaseHTTPRequestHandler):
 					self.send_error(403, 'Forbidden')
 					return
 
-			print 'path : ' + self.path
-			print 'fullpath : ' + fullpath
-
 			f = open(fullpath)
+
 			self.send_response(200)
-			readed = f.read()
-			self.wfile.write(readed)
+			#setting header
+			picmimelist = ['jpg', 'jpeg', 'png', 'gif'] # picture
+			for mime in picmimelist:
+				if( self.path.endswith('.' + mime) ):
+					self.send_header('Content-Type', 'image/' + mime)
+					break
+
+			musmimelist = ['mp3', 'flac', 'wav']
+			for mime in musmimelist:
+				if( self.path.endswith('.' + mime) ):
+					self.send_header('Content-Type', 'audio/' + mime)
+					break
+
+			self.end_headers()
+
+			self.wfile.write(f.read())
 			f.close()
 			return
 
@@ -66,14 +90,14 @@ class ServerConfig():
 	def __init__(self):
 		self.configListing()
 
-def main():
-	try:
-		server = HTTPServer(('localhost', 3080), Server)
-		print 'server start...'
-		server.serve_forever()
-	except KeyboardInterrupt:
-		server.socket.close()
-		print 'server stop...'
+#def main():
+#	try:
+#		server = HTTPServer(('localhost', 3080), Server)
+#		print 'server start...'
+#		server.serve_forever()
+#	except KeyboardInterrupt:
+#		server.socket.close()
+#		print 'server stop...'
 
-if __name__ == '__main__':
-	main()
+#if __name__ == '__main__':
+#	main()
